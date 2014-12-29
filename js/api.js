ba518709daedce811276b7079899c31835a6ff1f
Phoebe <femmebot@Phoebes-MacBook-Air.local>
2014-12-28 21:00:34 -0500

    listing Github projects I created

--- js/api.js
+++ js/api.js
@@ -38,15 +38,33 @@ $('.btn').click(function() {
   var url = "https://api.github.com/users/femmebot/repos";
   var repoName = "";
 
-  $.getJSON (url, function (response) {
+  $.getJSON (url, function ( response ) {
       $.each (response, function (index, repos) {
-        if (repos.fork === false) {
-          // repoName += '<p>' + repos.full_name + '</p>';
-          $('.text').append('<p>' + repos.full_name + '</p>');        }
-        // $('.text').text( repoName );
-      })
-  });
 
+        if (repos.fork === false) {
+          var commitURL = "https://api.github.com/repos/"+repos.full_name+"/commits"
+          // $('.text').append('<p>' + commitURL + '</p>');
+
+          var currentRepo = "";
+          
+          $.getJSON ( commitURL,function ( commitResponse ) {
+            $.each ( commitResponse, function (index, commitCount) {
+              if ( commitResponse.commit = repos.owner.login && repos.name != currentRepo) {
+                $('.text').append('<p>' + repos.name + '</p>');
+                currentRepo = repos.name;
+
+              } // end commits if statement
+            }) // end .each commitResponse loop
+
+          }) // end getJSON commitURL callback function
+
+          // repoName += '<p>' + repos.full_name + '</p>';
+          // $('.text').append('<p>' + repos.full_name + '</p>');        }
+        // $('.text').text( repoName );
+
+      } // end repos.fork if statement
+    }) // end .each response callback function
+  }) // end main response callback
 });
 
 /********************************/
