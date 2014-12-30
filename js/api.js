8503abf8a87838aacb1d9abf7ea98dc4b636c504
femmebot <femmebot@gmail.com>
2014-12-30 00:30:25 -0500

    fixed commit data

--- js/api.js
+++ js/api.js
@@ -36,7 +36,6 @@ $('.btn').click(function() {
   $('.text').text('loading . . .');
 
   var url = "https://api.github.com/users/femmebot/repos";
-  var repoName = "";
 
   $.getJSON (url, function ( response ) {
       $.each (response, function (index, repos) {
@@ -46,10 +45,11 @@ $('.btn').click(function() {
           // $('.text').append('<p>' + commitURL + '</p>');
 
           var currentRepo = "";
-          
+
           $.getJSON ( commitURL,function ( commitResponse ) {
-            $.each ( commitResponse, function (index, commitCount) {
-              if ( commitResponse.commit = repos.owner.login && repos.name != currentRepo) {
+            $.each ( commitResponse, function (index, commitData) {
+            // console.log( index + ' ' + commitData.commit.committer.name + ' ' + repos.owner.login );
+              if ( commitData.commit.committer.name === repos.owner.login && repos.name != currentRepo ) {
                 $('.text').append('<p>' + repos.name + '</p>');
                 currentRepo = repos.name;
 
